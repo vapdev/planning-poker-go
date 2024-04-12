906fb8fab03c884c18499440bcfe09a1d2e57095
Vitor <vpbohn@gmail.com>
2024-04-11 23:07:38 -0300

    Update CORS headers to allow all origins

--- cors.go
+++ cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func enableCors(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // replace with your domain
+		w.Header().Set("Access-Control-Allow-Origin", "*") // replace with your domain
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
